775b480f1969a1b329457c29661917ae9b4e91fe
VJKumar57 <154102218+VJKumar57@users.noreply.github.com>
2023-12-28 20:27:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ document.getElementById('menu-icon').addEventListener('click', function () {
     var menu = document.getElementById('menu');
     menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
 });
+
+function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById('main-content').innerHTML = this.responseText;
+        }
+    };
+    
+    // Adjust the path accordingly
+    xhttp.open("GET", page + '.html', true);
+    xhttp.send();
+}
+
+// Toggle the visibility of the menu on small screens
+document.getElementById('menu-icon').addEventListener('click', function () {
+    var menu = document.getElementById('menu');
+    menu.style.display = (menu.style.display === 'flex') ? 'none' : 'flex';
+});
